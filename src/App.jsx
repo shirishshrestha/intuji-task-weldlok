23f3b8809bb226677185a729e32f1dde1178e2ca
SHIRISH <shirish.shrestha07@gmail.com>
2023-12-05 16:25:39 +0545

    responsive of nav and hero section upto 1024px viewport

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <>
-      <header className="landing__page">
+      <header>
         <Navigation />
       </header>
       <main>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,36 +6,34 @@ const Header = () => {
   return (
     <>
       <section className="hero">
-        <div className="main__container">
-          <div className="hero__container">
-            <h1 className="hero__heading">
-              High quality, reliable and proven products and systems
-            </h1>
-            <h2 className="hero__subheading">
-              The Weldlok brand guarantees the right solution for all customers.
-            </h2>
-            <button className="hero__button">
-              <MdArrowOutward />
-              Find Out More
-            </button>
-            <div className="hero__animation">
-              <HeroCard
-                icon={"/images/industrial-icon.png"}
-                heading={"industrial"}
-                image={"/images/hero-industrial.png"}
-              />
-              <HeroCard
-                icon={"/images/civil-icon.png"}
-                heading={"civil"}
-                image={"/images/hero-civil.jpeg"}
-              />
-              <HeroCard
-                icon={"/images/architect.png"}
-                heading={"architectural"}
-                image={"/images/hero-architectural.jpeg"}
-                className="bg-top"
-              />
-            </div>
+        <div className="main__container hero__container">
+          <h1 className="hero__heading">
+            High quality, reliable and proven products and systems
+          </h1>
+          <h2 className="hero__subheading">
+            The Weldlok brand guarantees the right solution for all customers.
+          </h2>
+          <button className="hero__button">
+            <MdArrowOutward className="text-[1.5rem]" />
+            Find Out More
+          </button>
+          <div className="hero__animation">
+            <HeroCard
+              icon={"/images/industrial-icon.png"}
+              heading={"industrial"}
+              image={"/images/hero-industrial.png"}
+            />
+            <HeroCard
+              icon={"/images/civil-icon.png"}
+              heading={"civil"}
+              image={"/images/hero-civil.jpeg"}
+            />
+            <HeroCard
+              icon={"/images/architect.png"}
+              heading={"architectural"}
+              image={"/images/hero-architectural.jpeg"}
+              className="bg-top"
+            />
           </div>
         </div>
       </section>
